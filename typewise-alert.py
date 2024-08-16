e34f78116709e5bb658ba1cd26db7d794b98d279
MPM3KOR <manjularparmar@gmail.com>
2024-08-16 19:59:23 +0530

    Update typewise-alert.py

--- typewise-alert.py
+++ typewise-alert.py
@@ -1,47 +1,51 @@
+from abc import ABC, abstractmethod
 
+# Breach classification strategies
+class Cooling_Type(ABC):
+    @abstractmethod
+    def classify_temperature_breach(self, temperatureInC):
+        pass
 
-def infer_breach(value, lowerLimit, upperLimit):
-  if value < lowerLimit:
-    return 'TOO_LOW'
-  if value > upperLimit:
-    return 'TOO_HIGH'
-  return 'NORMAL'
+class Passive_Cooling(Cooling_Type):
+    def classify_temperature_breach(self, temperatureInC):
+        return infer_breach(temperature_in_c, 0, 35)
 
+class Hi_Active_Cooling(Cooling_Type):
+    def classify_temperature_breach(self, temperatureInC):
+        return infer_breach(temperature_in_c, 0, 45)
 
-def classify_temperature_breach(coolingType, temperatureInC):
-  lowerLimit = 0
-  upperLimit = 0
-  if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 35
-  elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 45
-  elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
-    upperLimit = 40
-  return infer_breach(temperatureInC, lowerLimit, upperLimit)
+class Med_Active_Cooling(Cooling_Type):
+    def classify_temperature_breach(self, temperatureInC):
+        return infer_breach(temperature_in_c, 0, 40)
 
+# Alert strategies
+class Send_Alert(ABC):
+    @abstractmethod
+    def Send_Alert(self, breach_type):
+        pass
 
-def check_and_alert(alertTarget, batteryChar, temperatureInC):
-  breachType =\
-    classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
+class Alert_to_Controller(Send_Alert):
+    def Send_Alert(self, breach_type):
+        header = 0xfeed
+        print(f'{header}, {breach_type}')
 
+class Alert_to_Email(Send_Alert):
+    def Send_Alert(self, breach_type):
+        recipient = "a.b@c.com"
+        if breach_type == 'TOO_LOW':
+            print(f'To: {recipient}')
+            print('Hi, the temperature is too low')
+        elif breach_type == 'TOO_HIGH':
+            print(f'To: {recipient}')
+            print('Hi, the temperature is too high')
 
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
+# Core function using strategies
+def check_and_alert(alert_strategy, cooling_type_strategy, temperatureInC):
+    breach_type = cooling_type_strategy.classify_temperature_breach(temperatureInC)
+    alert_strategy.send_alert(breach_type)
 
+# Usage
+cooling_type = Hi_Active_Cooling() 
+alert = Alert_to_Controller()  
 
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
+check_and_alert(alert, cooling_type, 50)
