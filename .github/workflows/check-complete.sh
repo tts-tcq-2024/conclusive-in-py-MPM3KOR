e8e906671c75112d457918f407ad7fdc13cf5d69
MPM3KOR <manjularparmar@gmail.com>
2024-08-19 14:48:38 +0530

    Update check-complete.sh

--- .github/workflows/check-complete.sh
+++ .github/workflows/check-complete.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-if grep -q _enter *.md; then
-  echo "Replace all text having _enter with your input"
-  exit 1
-fi
+for file in *.md; do
+  if grep -q "_enter" "$file"; then
+    echo "Replace all text having _enter with your input"
+    exit 1
+  fi
 done
